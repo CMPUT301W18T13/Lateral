e73b969754581c1c164b8fa3ebfc3422d6283b53
Cole Merkosky <cjmerkos@ualberta.ca>
2018-03-07 10:32:48 -0700

    Added all tasks, add task, and user profile screens

---
+++ app/src/main/java/com/lateral/lateral/activity/MyTaskViewActivity.java
@@ -0,0 +1,15 @@
+package com.lateral.lateral.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lateral.lateral.R;
+
+public class MyTaskViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_task_view);
+    }
+}
