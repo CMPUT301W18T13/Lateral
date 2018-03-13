90e18c7ac0609794fab3381a48fcc7fd681625a3
Devon Upton <doupton@ualberta.ca>
2018-03-13 01:27:59 -0600

    TaskViewActivity roughly works

--- app/src/main/java/com/lateral/lateral/activity/TaskViewActivity.java
+++ app/src/main/java/com/lateral/lateral/activity/TaskViewActivity.java
@@ -3,6 +3,9 @@ package com.lateral.lateral.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.lateral.lateral.R;
 import com.lateral.lateral.model.Task;
@@ -13,9 +16,23 @@ import com.lateral.lateral.service.implementation.DefaultBidService;
 import com.lateral.lateral.service.implementation.DefaultTaskService;
 import com.lateral.lateral.service.implementation.DefaultUserService;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+
 public class TaskViewActivity extends AppCompatActivity {
 
     public static final String EXTRA_TASK_ID = "com.lateral.lateral.TASK_ID";
+
+    private String taskID;
+
+    // UI elements
+    private TextView currentBid;
+    private TextView title;
+    private TextView username;
+    private TextView date;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +41,32 @@ public class TaskViewActivity extends AppCompatActivity {
 
         // Get the task id
         Intent taskIntent = getIntent();
-        String taskID = taskIntent.getStringExtra(EXTRA_TASK_ID);
+        this.taskID = taskIntent.getStringExtra(EXTRA_TASK_ID);
         // TODO: Handle null id
 
+        currentBid = findViewById(R.id.task_view_current_bid);
+        title = findViewById(R.id.task_view_title);
+        username = findViewById(R.id.task_view_username);
+        date = findViewById(R.id.task_view_date);
+        description = findViewById(R.id.task_view_description);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Task task = loadTask(taskID);
+
+        BigDecimal lowest = task.getLowestBid().getAmount();
+        currentBid.setText(NumberFormat.getCurrencyInstance().format(lowest));
+        title.setText(task.getTitle());
+        username.setText(task.getRequestingUser().getUsername());
+        DateFormat df = new SimpleDateFormat("MMM dd yyyy");
+        date.setText(df.format(task.getDate()));
+        description.setText(task.getDescription());
 
     }
 
-    // TODO: Probably move to a controller
     private Task loadTask(String taskID){
         TaskService taskService = new DefaultTaskService();
         UserService userService = new DefaultUserService();
@@ -39,5 +75,9 @@ public class TaskViewActivity extends AppCompatActivity {
         task.setRequestingUser(userService.getById(task.getRequestingUserId()));
         task.setLowestBid(bidService.getLowestBid(task.getId()));
         return task;
+    }
+
+    public void onBidButtonClick(View v){
+        // TODO: Place bid via dialog box
     }
 }
