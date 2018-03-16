b1034c82bbece0097dc9d469cc8d8551afb3342d
Nicholas Whitefield <Nick@Nicholass-MacBook-Pro.local>
2018-03-16 15:31:31 -0600

    refactoring commit

--- app/src/main/java/com/lateral/lateral/activity/AllTasksViewActivity.java
+++ app/src/main/java/com/lateral/lateral/activity/AllTasksViewActivity.java
@@ -37,46 +37,65 @@ https://developer.android.com/guide/topics/search/search-dialog.html#LifeCycle
 
 // TODO fix white bar at the top of this activity --> appeared on my original pull
 // TODO clicking seems to work but test more --> pass intents
-public class AllTasksViewActivity extends AppCompatActivity {
+public class AllTasksViewActivity extends TaskRecyclerViewActivity {
+//
+//    private RecyclerView.Adapter mAdapter;
+//    private ArrayList<Task> matchingTasks;
 
-    private RecyclerView.Adapter mAdapter;
-    private ArrayList<Task> matchingTasks;
-    //private int count = 0;
+    @Override
+    protected int getResourceLayoutID(){
+        return R.layout.activity_all_tasks_view;
+    }
+
+    @Override
+    protected int getRecyclerListID() {
+        return R.id.taskViewList;
+    }
+
+    @Override
+    protected Context currentActivityContext(){
+        return AllTasksViewActivity.this;
+    }
+
+    @Override
+    protected Class targetClass() {
+        return TaskViewActivity.class;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        RecyclerView mRecyclerView;
-        RecyclerView.LayoutManager mLayoutManager;
+//        RecyclerView mRecyclerView;
+//        RecyclerView.LayoutManager mLayoutManager;
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_tasks_view);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        matchingTasks = new ArrayList<Task>();
-
-        // defining recycler view
-        mRecyclerView = findViewById(R.id.taskViewList);
-        mRecyclerView.setHasFixedSize(true);
-
-        ItemClickSupport.addTo(mRecyclerView)
-                .setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
-                    @Override
-                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
-                        Log.d("ITEM CLICKED", "item " + (matchingTasks.get(position).getId()));
-                        Intent viewTaskIntent = new Intent(AllTasksViewActivity.this, TaskViewActivity.class);
-                        viewTaskIntent.putExtra(EXTRA_TASK_ID, (matchingTasks.get(position).getId()));
-                        startActivity(viewTaskIntent);
-                    }
-                });
-
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new TaskRowAdapter(matchingTasks);
-        mRecyclerView.setAdapter(mAdapter);
-
+//        setContentView(R.layout.activity_all_tasks_view);
+//
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        matchingTasks = new ArrayList<Task>();
+//
+//        // defining recycler view
+//        mRecyclerView = findViewById(R.id.taskViewList);
+//        mRecyclerView.setHasFixedSize(true);
+//
+//        ItemClickSupport.addTo(mRecyclerView)
+//                .setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+//                        Log.d("ITEM CLICKED", "item " + (matchingTasks.get(position).getId()));
+//                        Intent viewTaskIntent = new Intent(AllTasksViewActivity.this, TaskViewActivity.class);
+//                        viewTaskIntent.putExtra(EXTRA_TASK_ID, (matchingTasks.get(position).getId()));
+//                        startActivity(viewTaskIntent);
+//                    }
+//                });
+//
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mAdapter = new TaskRowAdapter(matchingTasks);
+//        mRecyclerView.setAdapter(mAdapter);
+//
         // check how we got here
         handleIntent(getIntent());
 
@@ -86,24 +105,25 @@ public class AllTasksViewActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        SearchView searchView;
-
-        // Inflate the options menu from XML
-        getMenuInflater().inflate(R.menu.task_view_menu, menu);
-
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        super.onCreateOptionsMenu(menu);
+//
+//        SearchView searchView;
+//
+//        // Inflate the options menu from XML
+//        getMenuInflater().inflate(R.menu.task_view_menu, menu);
+//
+//        // Get the SearchView and set the searchable configuration
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+//        // Assumes current activity is the searchable activity
+//        // (although need this line in each activity for some reason)
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        //searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
         return true;
     }
 
 
-    @Override
+    //@Override
     protected void onNewIntent(Intent intent){
         setIntent(intent);
         handleIntent(intent);
@@ -112,36 +132,33 @@ public class AllTasksViewActivity extends AppCompatActivity {
     // meat and potatoes of the search
     private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            //Log.d("SEARCH", "Inside AllTasksViewActivity");
-            //Toast.makeText(getApplicationContext(), Integer.toString(count), Toast.LENGTH_LONG).show();     //Toast for debugging
             clearList();
             String query = intent.getStringExtra(SearchManager.QUERY);
-            //Log.d("QUERY", "" + query);
-            // implement some sort of exception handler (no internet access crashes app)
+            // TODO handle exception (no internet access crashes app)
             /*
             Leave for now
             String jsonQuery = "{\"query\": {\"multi_match\": {\"title\": {\"query\" : \"" + searchField + "\"," +
                 " \"fields\" : [\"title^3\", \"description\"]}}}}";
              */
             returnMatchingTask(query);
-            //count ++;
-
-
         }
 
     }
 
-    private void addTasks(ArrayList<Task> returnedTasks) {
-        matchingTasks.addAll(returnedTasks);
-        mAdapter.notifyItemInserted(matchingTasks.size() - 1);
+//    @Override
+//    public void addTasks(ArrayList<Task> returnedTasks) {
+//        super.addTasks(returnedTasks);
+////        matchingTasks.addAll(returnedTasks);
+////        mAdapter.notifyItemInserted(matchingTasks.size() - 1);
+//
+//    }
 
-    }
-
-    private void clearList() {
-        final int size = matchingTasks.size();
-        matchingTasks.clear();
-        mAdapter.notifyItemRangeRemoved(0, size);
-    }
+//    public void clearList() {
+//        super.clearList();
+////        final int size = matchingTasks.size();
+////        matchingTasks.clear();
+////        mAdapter.notifyItemRangeRemoved(0, size);
+//    }
 
     private void returnMatchingTask(String query) {
         DefaultTaskService taskService = new DefaultTaskService();
--- app/src/main/java/com/lateral/lateral/activity/TaskRecyclerViewActivity.java
+++ app/src/main/java/com/lateral/lateral/activity/TaskRecyclerViewActivity.java
@@ -1,12 +1,10 @@
 package com.lateral.lateral.activity;
 
+
 import android.app.SearchManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,19 +17,37 @@ import android.view.View;
 import com.lateral.lateral.R;
 import com.lateral.lateral.model.Task;
 import com.lateral.lateral.service.ItemClickSupport;
-import com.lateral.lateral.service.implementation.DefaultTaskService;
 
 import java.util.ArrayList;
 
-import static com.lateral.lateral.MainActivity.LOGGED_IN_USER;
 import static com.lateral.lateral.activity.TaskViewActivity.EXTRA_TASK_ID;
 
+public abstract class TaskRecyclerViewActivity extends AppCompatActivity {
 
-public class RequestedTasksViewActivity extends AppCompatActivity {
-    private RecyclerView.Adapter mAdapter;
-    private ArrayList<Task> matchingTasks;
+    public RecyclerView.Adapter mAdapter;
+    public ArrayList<Task> matchingTasks;
 
-    private String thisUserID = LOGGED_IN_USER;             // class spec       // for testing
+
+    public RecyclerView.Adapter getmAdapter(){
+        return this.mAdapter;
+    }
+
+    public ArrayList<Task> getMatchingTasks() {
+        return this.matchingTasks;
+    }
+
+    public void setmAdapter(RecyclerView.Adapter newAdapter){
+        this.mAdapter = newAdapter;
+    }
+
+    public void setMatchingTasks(ArrayList<Task> newMatchingTasks) {
+        this.matchingTasks = newMatchingTasks;
+    }
+
+    protected abstract int getResourceLayoutID();
+    protected abstract int getRecyclerListID();
+    protected abstract Context currentActivityContext();
+    protected abstract Class targetClass();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +56,7 @@ public class RequestedTasksViewActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager;
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_requested_tasks_view);
+        setContentView(getResourceLayoutID());
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -48,15 +64,15 @@ public class RequestedTasksViewActivity extends AppCompatActivity {
         matchingTasks = new ArrayList<Task>();
 
         // defining recycler view
-        mRecyclerView = findViewById(R.id.requestedTaskViewList);
+        mRecyclerView = findViewById(getRecyclerListID());
         mRecyclerView.setHasFixedSize(true);
 
         ItemClickSupport.addTo(mRecyclerView)
                 .setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
                     @Override
                     public void onItemClicked(RecyclerView recyclerView, int position, View v) {
-                        Log.d("ITEM CLICKED", "AN ITEM WAS CLICKED");
-                        Intent viewTaskIntent = new Intent(RequestedTasksViewActivity.this, MyTaskViewActivity.class);
+                        Log.d("ITEM CLICKED", "item " + (matchingTasks.get(position).getId()));
+                        Intent viewTaskIntent = new Intent(currentActivityContext(), targetClass());
                         viewTaskIntent.putExtra(EXTRA_TASK_ID, (matchingTasks.get(position).getId()));
                         startActivity(viewTaskIntent);
                     }
@@ -66,21 +82,6 @@ public class RequestedTasksViewActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new TaskRowAdapter(matchingTasks);
         mRecyclerView.setAdapter(mAdapter);
-
-
-        // Move to create new task activity when fab pressed
-        FloatingActionButton addNewTaskFab = (FloatingActionButton) findViewById(R.id.addNewTaskFab);
-        addNewTaskFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent AddTaskIntent = new Intent(RequestedTasksViewActivity.this, AddEditTaskActivity.class);
-                startActivity(AddTaskIntent);
-            }
-        });
-
-        // display requested tasks from this user
-        returnMatchingTasks(thisUserID);
-
     }
 
 
@@ -102,22 +103,17 @@ public class RequestedTasksViewActivity extends AppCompatActivity {
         return true;
     }
 
-    private void addTasks(ArrayList<Task> returnedTasks) {
+
+    public void addTasks(ArrayList<Task> returnedTasks) {
         matchingTasks.addAll(returnedTasks);
         mAdapter.notifyItemInserted(matchingTasks.size() - 1);
 
     }
 
-    private void clearList() {
+    public void clearList() {
         final int size = matchingTasks.size();
         matchingTasks.clear();
         mAdapter.notifyItemRangeRemoved(0, size);
     }
-
-    private void returnMatchingTasks(String query) {
-        DefaultTaskService taskService = new DefaultTaskService();
-        addTasks(taskService.getAllTasksByRequesterID(query));
-    }
-
 
 }
